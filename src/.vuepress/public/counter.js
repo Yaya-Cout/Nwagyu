5fc088a0d17c6049450650ecc5aea2951877587e
Yaya-Cout <yaya.cout@ik.me>
2026-03-31 16:22:24 +0200

    [Counter] Use async function to prevent blocking page load

--- src/.vuepress/public/counter.js
+++ src/.vuepress/public/counter.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", async function() {
     // Simple page view counter with no HTTP logs, logging daily page view in CSV file for Grafana plotting
     let request = new XMLHttpRequest()
     request.open('GET', 'https://counter.yann.n1n1.xyz:8080/numworks-nwagyu-website-load', true)
